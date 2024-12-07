fff527af42476d8175d49cc4ebf99ac688bd90dd
Mary <marynevermann@gmail.com>
2024-12-07 04:38:02 +0100

    make sure the prompt work correctly

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 function generateDefination(event){
     event.preventDefault();
-   let definitionElement = document.querySelector("#definition");
 
-   new Typewriter('#definition', {
-    strings: "la definition de fleur est...",
-    autoStart: true,
-    delay: 1,
-    cursor: null
-  });
-  
+    let userInputValue= document.querySelector("#user-input");
+    let apikey ="1b3cfb66ad014a3fo55df2e890f445t9";
+    
+    let prompt = `user instructions: Generate a short and precise definition of the ${userInputValue.value}`;
+    let context= `You are an universal Dictionary.please identify first the language of ${userInputValue.value}. Then make sure to follow the user instructions. provide a precise title about the language you are about to use for the definition. make sure to give the definition in both english and then in the language of ${userInputValue.value}. please don't write about html just display in basic html.`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+    
+   axios.get(apiUrl).then(showDefination);
+}
+function showDefination(response){
+    new Typewriter('#definition', {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: null,
+      });
+      
 }
 
 
 let defintitionFormElement = document.querySelector("#definition-generator-form");
 defintitionFormElement.addEventListener("submit", generateDefination);
+
