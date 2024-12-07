c7efe1217352f1ac119b32d83b3077d042434e6e
Mary <marynevermann@gmail.com>
2024-12-07 07:48:10 +0100

    added a loading animation

--- src/index.js
+++ src/index.js
@@ -8,16 +8,23 @@ function generateDefination(event){
     let context= `You are an universal Dictionary.please identify first the language of ${userInputValue.value}. Then make sure to follow the user instructions. provide a precise title about the language you are about to use for the definition. make sure to give the definition in both english and then in the language of ${userInputValue.value}. please ensure to  display in basic html.`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
     
+    //show the loading emoji with animation
+    let definitionAnimation= document.querySelector("#definition");
+    definitionAnimation.innerHTML='<p class="loading">⏳ </p>Loading...'
     //default display structure
     let definitionElement= document.querySelector("#definition");
     definitionElement.classList.remove("hidden");
     //clear the previous definition to prevent overlap
-    let inputRemover = document.querySelector("#definition");
-    inputRemover.innerHTML="";
-   //show the result
+    //let inputRemover = document.querySelector("#definition");
+    //inputRemover.innerHTML="";
+   //make the api request
     axios.get(apiUrl).then(showDefination);
 }
 function showDefination(response){
+    //remove the loading emoji when response is received
+    let definitionAnimationRemover= document.querySelector("#definition");
+    definitionAnimationRemover.innerHTML='';
+    //type in the result
     new Typewriter('#definition', {
         strings: response.data.answer,
         autoStart: true,
