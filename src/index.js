055413f6790f09c245004751117843da06e48d00
Mary <marynevermann@gmail.com>
2024-12-07 08:41:44 +0100

    changed the prompt

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ function generateDefination(event){
     
     let prompt = `user instructions: Generate a short and precise definition of the ${userInputValue.value}`;
     let context= `You are an universal Dictionary.please identify first the language of ${userInputValue.value}. Then make sure to follow the user instructions.
-     provide a precise title about which language you are using for the definition. 
-     make sure to give the definition in both english and then in the language of ${userInputValue.value}. if the language of ${userInputValue.value} is already in english only provide the definition in english. 
-     please ensure to only display in basic html. make sure to not write anything html`;
+     provide a precise title about which language you are using for the definition. make sure to not write anything html but display only in basic html.
+    please give the definition in both english and then in the language of ${userInputValue.value}. if the language of ${userInputValue.value} is already in english only provide the definition in english. 
+    `;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
     
     //show the loading emoji with animation
