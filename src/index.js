e2531c1ff15be42e58f10fdbc8a9789dc109abb2
Mary <marynevermann@gmail.com>
2024-12-07 05:59:19 +0100

    add a input remover for old input

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ function generateDefination(event){
     let context= `You are an universal Dictionary.please identify first the language of ${userInputValue.value}. Then make sure to follow the user instructions. provide a precise title about the language you are about to use for the definition. make sure to give the definition in both english and then in the language of ${userInputValue.value}. please don't write about html just display in basic html.`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
     
-   axios.get(apiUrl).then(showDefination);
+   //clear the previous definition to prevent overlap
+    let inputRemover = document.querySelector("#definition");
+    inputRemover.innerHTML="";
+   //show the result
+    axios.get(apiUrl).then(showDefination);
 }
 function showDefination(response){
     new Typewriter('#definition', {
         strings: response.data.answer,
         autoStart: true,
-        delay: 1,
+        delay: 40,
         cursor: null,
       });
       
